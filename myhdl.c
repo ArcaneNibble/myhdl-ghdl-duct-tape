f4437a1b1543e51f94756be910b1b7635f117870
Robert Ou <rqou@robertou.com>
2016-07-15 12:48:04 -0700

    Iterate through nets

--- myhdl.c
+++ myhdl.c
@@ -7,6 +7,11 @@
 
 static vpiHandle from_myhdl_net_handle[MAXARGS];
 static vpiHandle to_myhdl_net_handle[MAXARGS];
+int from_myhdl_net_count, to_myhdl_net_count;
+
+static inline int startswith(const char *s, const char *prefix) {
+    return strncmp(s, prefix, strlen(prefix)) == 0;
+}
 
 static PLI_INT32 endofcompile_callback(p_cb_data cb_data) {
     vpiHandle top_iter = vpi_iterate(vpiModule, NULL);
@@ -17,8 +22,29 @@ static PLI_INT32 endofcompile_callback(p_cb_data cb_data) {
     vpiHandle net_iter = vpi_iterate(vpiNet, top_module);
     vpiHandle net_handle;
     while ((net_handle = vpi_scan(net_iter))) {
-        char *n = vpi_get_str(vpiName, net_handle);
-        fprintf(stderr, "net %s\n", n);
+        char *name = vpi_get_str(vpiName, net_handle);
+        fprintf(stderr, "net %s\n", name);
+        if (startswith(name, "to_myhdl_")) {
+            if (to_myhdl_net_count == MAXARGS) {
+                vpi_printf("ERROR: Too many to_myhdl nets\n");
+                vpi_control(vpiFinish, 1);
+            }
+            to_myhdl_net_handle[to_myhdl_net_count++] = net_handle;
+        } else if (startswith(name, "from_myhdl_")) {
+            if (from_myhdl_net_count == MAXARGS) {
+                vpi_printf("ERROR: Too many from_myhdl nets\n");
+                vpi_control(vpiFinish, 1);
+            }
+            from_myhdl_net_handle[from_myhdl_net_count++] = net_handle;
+        }
+    }
+
+    // debug
+    for (int i = 0; i < to_myhdl_net_count; i++) {
+        fprintf(stderr, "to_myhdl handle %p\n", to_myhdl_net_handle[i]);
+    }
+    for (int i = 0; i < from_myhdl_net_count; i++) {
+        fprintf(stderr, "from_myhdl handle %p\n", from_myhdl_net_handle[i]);
     }
 
     return(0);
